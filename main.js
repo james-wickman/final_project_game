523ad5e293fa5ea9b4852cc02d3664db694954a3
nauticalnova <nauticalnova@hotmail.com>
2017-03-03 14:12:51 -0700

    added shooting with angles

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 var score = 0;
 var scoreText;
 var jumpTimer = 0;
-
-
+var s;
+var ledge;
+var sprite;
+var weapon;
+var cursors;
+var fireButton;
+var bullets;
+var platforms;
+var bulletTimer = 0;
+var direction_facing = 'left';
 
 function preload() {
 
+	game.load.image('bullet', 'master/examples/assets/sprites/bullet.png');
 	game.load.atlasJSONHash('bot', 'master/examples/assets/sprites/running_bot.png', 'master/examples/assets/sprites/running_bot.json');
     game.load.image('sky', 'master/examples/assets/skies/sky1.png');
     game.load.image('ground', 'master/examples/assets/sprites/platform.png');
@@ -15,19 +24,51 @@ function preload() {
 }
 
 function create() {
+    game.world.setBounds(-30, -30, 4000, 2000);
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.add.sprite(0, 0, 'sky');
+
+    game.add.sprite(0, 300, 'sky');
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
 
     platforms = game.add.group();
+    platforms.enableBody = true;
 
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    var ground = platforms.create(0, game.world.height - 54, 'ground');
     ground.scale.setTo(2, 2);
+    ground.body.immovable = true;
 
-    var ledge = platforms.create(400, 400, 'ground');
 
-    ledge = platforms.create(-150, 250, 'ground');
-    
-    s = game.add.sprite(game.world.centerX, game.world.centerY, 'bot');
+    ledge = platforms.create(0, 800, 'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(300, 700, 'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(350, 800, 'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(550, 800, 'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(1000, 800, 'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(2550, 800, 'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(1350, 800, 'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(2050, 800, 'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(3050, 800, 'ground');
+    ledge.body.immovable = true;
+
+    s = game.add.sprite(200, 700, 'bot');
     s.anchor.setTo(0.5, 0.5);
     s.scale.setTo(2, 2);
 
@@ -37,24 +78,31 @@ function create() {
     game.physics.enable(s, Phaser.Physics.ARCADE);
     s.body.collideWorldBounds = true;
 
-    s.body.bounce.set(0.1);
+    s.body.bounce.set(0.03);
 
     s.body.gravity.set(0, 380);
+    game.camera.follow(s);
+    game.camera.deadzone = new Phaser.Rectangle(150, 200, 400, 200);
+    game.physics.arcade.enable([s, ledge]);
+    ledge.body.allowGravity = false;
+	ledge.body.immovable = true;
+
+	bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(30, 'bullet');
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 1);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true)
+
+	fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
 }
 
 function update() {
-	flip = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-	flip.onDown.add(shootBullet, this);
-	function shootBullet() { 
-		s.scale.x *= -1;
-
-	}
-	// game.input.keyboard.onDown.add(processKey, this);
-	// function processKey(key) {
-	// if (key.keyCode == Phaser.RIGHT) {    
-	// 	s.scale.x *= -1;
-	// }} 
-
+	var hitPlatform = game.physics.arcade.collide(s, platforms);
+	s.body.velocity.x = 0;
 
 	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
     {
@@ -62,37 +110,84 @@ function update() {
 
     	}else {
     		s.scale.x *= -1;
+    		direction_facing = 'left';
     	}
     	s.animations.play('run', 10, true);
-        s.x -= 4;
+        s.body.velocity.x = -350;
+
     } 
     else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
     {
     	if (s.scale.x == 2) {
     		s.scale.x *= -1;
+    		direction_facing = 'right'
     	}else {
     		
     	}
     	s.animations.play('run', 10, true);
-        s.x += 4;
+        s.body.velocity.x = 350;
+
     }
     else {
     	s.animations.stop('run', 10, false);
     }
 
-    if (game.input.keyboard.isDown(Phaser.Keyboard.UP) && s.body.onFloor() && game.time.now > jumpTimer)
+    if (game.input.keyboard.isDown(Phaser.Keyboard.UP) && s.body.touching.down && hitPlatform && game.time.now > jumpTimer)
     {
-        s.body.velocity.y = -450;
-        jumpTimer = game.time.now + 750;
+        s.body.velocity.y = -350;
+        jumpTimer = game.time.now + 350;
 
     }
+    if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
 
+    }
+    game.physics.arcade.collide(s, ledge);
 
-
+    if (fireButton.isDown)
+    {
+        fireBullet();
+    }
 }
 
-function render() {
+function fireBullet() {
+    //  Grab the first bullet we can from the pool
+    if (game.time.now > bulletTimer) {
+    	var BULLET_SPEED = 700;
+        var BULLET_SPACING = 450;
+        //  Grab the first bullet we can from the pool
+        var bullet = bullets.getFirstExists(false);
 
+        if (bullet)
+        {
+            //  And fire it
+            //  Make bullet come out of tip of ship with right angle
+            var bulletOffset = 20 * Math.sin(game.math.degToRad(s.angle));
+            bullet.reset(s.x + bulletOffset, s.y);
+            bullet.angle = s.angle;
+            if(direction_facing == 'right') {
+            	game.physics.arcade.velocityFromAngle(bullet.angle, BULLET_SPEED, bullet.body.velocity);
+            }
+            if(direction_facing == 'left') {
+            	game.physics.arcade.velocityFromAngle(bullet.angle, -BULLET_SPEED, bullet.body.velocity);
+            }
+            if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+
+            	bullet.body.velocity.y = -200;
+            	console.log(bullet.body.velocity.y)
+            }
+            if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+            	bullet.body.velocity.y = 200;
+            }
+
+            bulletTimer = game.time.now + BULLET_SPACING;
+        }
+     }
+ }
+	    
+
+function render() {
+	var zone = game.camera.deadzone;
     game.debug.spriteInfo(s, 20, 32);
+    // weapon.debug();
 
 }
